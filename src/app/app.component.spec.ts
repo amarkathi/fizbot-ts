73c997561107cf078e82ec8cb4b0d25dab2eb0b1
amarkathi <41131206+amarkathi@users.noreply.github.com>
2018-09-26 15:02:48 -0700

    Add files via upload
    latest modifed for the project.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -13,15 +13,15 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'fzb'`, async(() => {
+  it(`should have as title 'mybootstrapproject'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('fzb');
+    expect(app.title).toEqual('mybootstrapproject');
   }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to fzb!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to mybootstrapproject!');
   }));
 });
