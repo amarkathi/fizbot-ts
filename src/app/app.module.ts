80fb4e05bda0c153dba4f18a74c17f4872caeaa9
Amareswara kathi <amarkathi@gmail.com>
2018-11-08 10:38:31 -0800

    Modified CSS for the responses part of the bot

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
+import {AppComponent} from './app.component';
+import {HeaderComponent } from './header/header.component';
 import { ResponsesComponent } from './responses/responses.component';
-import {UserQuestionComponent} from "./user-question/user-question.component";
-import {HttpModule} from "@angular/http";
-import {HttpClientModule} from "@angular/common/http";
-import {WatsonAPIService} from "./shared/services/watson-api.service";
+import { UserQuestionComponent } from './user-question/user-question.component';
+import { HttpClientModule } from '@angular/common/http';
+import { WatsonAPIService } from './shared/services/watson-api.service';
+import { RouterModule, Routes } from '@angular/router';
+
+
+const appRoutes: Routes = [
+  {path: 'fisbot', component: AppComponent}
+
+];
 
 @NgModule({
   declarations: [
@@ -18,9 +24,14 @@ import {WatsonAPIService} from "./shared/services/watson-api.service";
   ],
   imports: [
     HttpClientModule,
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [WatsonAPIService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
